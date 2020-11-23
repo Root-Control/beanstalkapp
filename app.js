b4eedd53ab89cc60163a9b8acb9fc2dc5613c412
HiroPalacios <l2oot.control@gmail.com>
2020-11-23 17:42:01 -0500

    counter added

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const port = 8080
+let requestCounter = 0;
 
 app.get('/', (req, res) => {
   console.log(`Request received at ${new Date()}`)
+  requestCounter++;
   res.send('Hello World!');
+});
+
+app.get('/counter', (req, res) => {
+  res.json({
+    counter: requestCounter
+  });
 });
 
 app.listen(port, () => {
